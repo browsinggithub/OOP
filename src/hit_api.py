fe91d9a6fc071a1ed5cddf808e256478da0e198e
Cameron Arredondo <cam.arredondo@outlook.com>
2022-10-28 21:50:52 -0500

    add a readme

--- src/hit_api.py
+++ src/hit_api.py
@@ -22,12 +22,21 @@ class api:
         hit_api.text
         find_key = hit_api.json()
         print(f"{activity.capitalize()} event: {find_key['activity']}")
+        return 
+
+    def suggestion_for_each_activity(self, all):
+        hit_api = requests.get(self.url + activity)
+        hit_api.encoding = "utf-8"
+        hit_api.text
+        find_key = hit_api.json()
+        for all in activity:
+            print(f"{all.capitalize()} Event: {find_key['activity']}")
 
         
 
-
 activity = api("http://www.boredapi.com/api/activity?type=")
 
-activity.requests_bored_api_activity("recreational")
+activity.requests_bored_api_activity("education")
+
 
 # activity.iterate_through_activities()
