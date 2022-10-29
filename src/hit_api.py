dddf7393ac3c81cebb402f2dab45077f47212dbb
Cameron Arredondo <cam.arredondo@outlook.com>
2022-10-29 00:16:11 -0500

    fix logic

--- src/hit_api.py
+++ src/hit_api.py
@@ -1,11 +1,6 @@
 import requests
 
-class Bored:
-    def __init__(self, url):
-        self.url = url
-
-
-    activities = {
+activities = {
         "education",
         "recreational",
         "social",
@@ -17,32 +12,40 @@ class Bored:
         "busywork",
     }
 
-    def requests_bored_api_activity(self, activity):
+class APIs:
+    def __init__(self, url):
+        self.url = url
+
+
+    
+
+    def requests_bored_api_activity(self, subject):
         """Query the bored API to find something to do.  You 
         can change which argument you pass through this method with any of the strings in
         the activities list.  Choose an activity you feel like trying something new in."""
 
-        hit_api = requests.get(self.url + activity)
+        hit_api = requests.get(self.url + subject)
         hit_api.encoding = "utf-8"
         hit_api.text
         find_key = hit_api.json()
-        print(f"{activity.capitalize()} event: {find_key['activity']}")
+        print(f"{subject.capitalize()} event: {find_key['activity']}")
         return 
 
-    def suggestion_for_each_activity(self, all):
+    def suggestion_for_each_activity(self):
         """get a suggestion of something to do for any activity"""
-        hit_api = requests.get(self.url + activity)
-        hit_api.encoding = "utf-8"
-        hit_api.text
-        find_key = hit_api.json()
-        for all in activity:
+        for all in activities:
+            hit_api = requests.get(self.url + all)
+            hit_api.encoding = "utf-8"
+            hit_api.text
+            find_key = hit_api.json()
             print(f"{all.capitalize()} Event: {find_key['activity']}")
 
         
 
-activity = Bored("http://www.boredapi.com/api/activity?type=")
+activity = APIs("http://www.boredapi.com/api/activity?type=")
 
 activity.requests_bored_api_activity("education")
+activity.suggestion_for_each_activity()
 
 
 # activity.iterate_through_activities()
