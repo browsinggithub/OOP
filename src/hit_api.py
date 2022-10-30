79b1036a6694a0932f045a19db32d486505d6e7c
Cameron Arredondo <cam.arredondo@outlook.com>
2022-10-30 00:08:27 -0500

    saturday night refactor

--- src/hit_api.py
+++ src/hit_api.py
@@ -1,52 +1,94 @@
-from turtle import done
 import requests
+from requests.exceptions import Timeout
+import click
 
 activities = [
-        "education",
-        "recreational",
-        "social",
-        "diy",
-        "charity",
-        "cooking",
-        "relaxation",
-        "music",
-        "busywork",
+    "education",
+    "recreational",
+    "social",
+    "diy",
+    "charity",
+    "cooking",
+    "relaxation",
+    "music",
+    "busywork",
 ]
-class APIs:
 
 
-    def requests_bored_api_recreation(subject):
-        """Query the bored API to find something to do.  You 
-        can change which argument you pass through this method with any of the strings in
-        the activities list.  Choose an activity you feel like trying something new in."""
+def query(subject):
+    """Query the bored API to find something to do.  You
+    can change which argument you pass through this method with any of the strings in
+    the activities list.  Choose an activity you feel like trying something new in."""
+    try:
+        hit_api = requests.get(
+            "http://www.boredapi.com/api/activity?type=" + subject, timeout=5
+        )
+    except Timeout:
+        print("I waited for too long!")
+    else:
+        print("The API request got executed!")
 
-        hit_api = requests.get("http://www.boredapi.com/api/activity?type=" + subject)
         hit_api.encoding = "utf-8"
-        hit_api.text
+        find_key = hit_api.text
         find_key = hit_api.json()
-        print(f"{subject.capitalize()} event: {find_key['activity']}")
-
-    def suggestion_for_each_activity():
-        for all in activities:
-            APIs.requests_bored_api_recreation(all)
-            
-    def request_recreation():
-        APIs.requests_bored_api_recreation("recreational")
-
-    def request_social():
-        APIs.requests_bored_api_recreation("social")
-
-    def requests_diy():
-        APIs.requests_bored_api_recreation("diy")
-
-    def requests_charity()
+        print(f"{subject.capitalize()} idea: {find_key['activity']}")
 
 
+def suggestion_for_each_activity():
+    for every_activity in activities:
+        query(every_activity)
 
 
+@click.group()
+def cli():
+    pass
 
-suggestion_for_each_activity()   
-#request_for_recreation()
+
+@cli.command(name="edu", help="Ask for a random educational topic to learn")
+def request_education():
+    query("education")
 
 
-# activity.iterate_through_activities()
+@cli.command(name="rec", help="Ask for a random recreational (active) activity ideas")
+def request_recreation():
+    query("recreational")
+
+
+@cli.command(name="social", help="Ask for random social event ideas")
+def request_social():
+    query("social")
+
+
+@cli.command(name="diy", help="Ask for random DIY ideas")
+def requests_diy():
+    query("diy")
+
+
+@cli.command(name="charity", help="Ask for a random charity idea")
+def requests_charity():
+    query("charity")
+
+
+@cli.command(name="cook", help="Ask for random cooking ideas")
+def requests_cooking():
+    query("cooking")
+
+
+@cli.command(name="relax", help="Ask for ideas on how to relax ")
+def requests_relaxation():
+    query("relaxation")
+
+
+@cli.command(name="music", help="Ask for ideas related to music")
+def requests_music():
+    query("music")
+
+
+@cli.command(name="busy_work", help="Ask for ideas related to bsuy work")
+def requests_busywork():
+    query("busywork")
+
+
+if __name__ == "__main__":
+    print("Thanks for trying this out! -Cam")
+    cli()
